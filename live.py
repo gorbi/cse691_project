627e403bd39f74b1089a2894d27eb31cea226993
Nagaprasad Natarajaurs <nagaprasad@outlook.in>
2017-12-05 08:06:48 -0500

    Use local checkpoints & debug mode

--- live.py
+++ live.py
@@ -1,22 +1,25 @@
 from evaluate import ffwd_live
 import cv2
 
-cap = cv2.VideoCapture(0) # Capture video from camera
+cap = cv2.VideoCapture(0)  # Capture video from camera
 
 # Get the width and height of frame
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) + 0.5)
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) + 0.5)
 
 count = 0
+debug = False
 
 while cap.isOpened():
     ret, frame = cap.read()
     if ret:
 
-        frame = ffwd_live(frame, "/null", "/Users/nagaprasad/Downloads/fast-style-transfer-master/examples/style/udnie.ckpt")
+        frame = ffwd_live(frame, "/null", "ckpt/la_muse.ckpt")
 
-        cv2.imshow('frame', frame)
-        #cv2.imwrite('out'+str(count)+'.png', frame)
+        if debug:
+            cv2.imwrite('out' + str(count) + '.png', frame)
+        else:
+            cv2.imshow('frame', frame)
 
         count += 1
 
